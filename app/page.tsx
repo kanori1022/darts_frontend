d77704e4d9025acd53f287b004f608f0d9da753b
kanori1022 <i.like_96neko54@ezweb.ne.jp>
2025-05-18 17:30:58 +0900

    各種修正/追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
+import {
+  faBars,
+  faHeart,
+  faHouse,
+  faMagnifyingGlass,
+  faPlus,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// ここから検索
+// https://fontawesome.com/icons?t=packs#packs
+
+// 横並びは
+// flexを使います
+// https://tailwindcss.com/docs/flex
+
 export default function Home() {
   return (
     <div>
       <div className="h-screen">
-        <p className="text-3xl font-serif bg-[#333333] text-[#CCCCCC] p-2 text-center">
-          SakoDax Darts
+        <p className="bg-[#333333] p-5 flex justify-between">
+          <div className="text-3xl font-serif text-[#CCCCCC] pl-20">
+            Combine Darts
+          </div>
+          <div className="h-8 w-8 text-[#CCCCCC]">
+            <FontAwesomeIcon icon={faBars} />
+          </div>
         </p>
         <p className="my-10 text-3xl font-bold font-Noto Sans text-center">
           ログイン
@@ -27,6 +49,18 @@ export default function Home() {
           <button className="bg-[#3B82F6] mt-10 py-4 w-full whitespace-nowrap rounded-sm text-[#FFFFFF]">
             ログイン
           </button>
+        </div>
+        <div className="bg-neutral-100">
+          <div className="py-43"></div>
+          <div className="bg-[#333333] px-8 py-6 ">
+            <div className="text-[#828282] flex gap-10 bg-[#333333]">
+              <FontAwesomeIcon icon={faHouse} />
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+              <FontAwesomeIcon icon={faPlus} />
+              <FontAwesomeIcon icon={faHeart} />
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
