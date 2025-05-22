807a72376f87ccc33682060bb4d524b340cb58b0
kanori1022 <i.like_96neko54@ezweb.ne.jp>
2025-05-22 19:30:22 +0900

    ヘッダー・フッター等の追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-
 import { Geist, Geist_Mono } from "next/font/google";
+import { Footer } from "./components/Footer/page";
+import { Header } from "./components/Header/page";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased lg:w-1/3 mx-auto`}
       >
+        <Header />
         <div className="bg-[#F1F6F7]">{children}</div>
+        <Footer />
       </body>
     </html>
   );
