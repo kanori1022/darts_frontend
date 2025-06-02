356244b8f21e9faf38c78fed396d24d64557d72b
kanori1022 <i.like_96neko54@ezweb.ne.jp>
2025-06-02 23:02:50 +0900

    home画面更新

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,35 @@
 export default function home() {
   return (
     <div>
-      <div className="pl-3 pr-3 pt-3 pb-3 bg-white mb-70">
-        <div className="人気ランキング font-bold">人気ランキング</div>
+      <div className="pb-3">
+        <div className="p-3 font-bold bg-white">人気ランキング</div>
+        <div className="flex overflow-scroll gap-3 pt-3">
+          <img
+            className="w-50 h-30 shadow-sm border rounded-lg"
+            src="sampl1.png"
+          />
+          <img
+            className="w-50 h-30 shadow-sm border rounded-lg"
+            src="sampl2.jpg"
+          />
+          <img
+            className="w-50 h-30 shadow-sm border rounded-lg"
+            src="sampl3.jpg"
+          />
+          <img
+            className="w-50 h-30 shadow-sm border rounded-lg"
+            src="sampl4.jpg"
+          />
+          <img
+            className="w-50 h-30 shadow-sm border rounded-lg"
+            src="sampl5.jpg"
+          />
+        </div>
       </div>
-      {/* ここにファイルインポートの処理 */}
+      {/* 横スクロールをできる処理を追加 */}
 
-      <div className="pl-3 pr-3 pt-3 pb-3 bg-white mb-70">
-        <div className="新着一覧 font-bold">新着一覧</div>
+      <div className="pb-3">
+        <div className="p-3 font-bold  bg-white">新着一覧</div>
       </div>
       <div className="bg-neutral-100"></div>
     </div>
