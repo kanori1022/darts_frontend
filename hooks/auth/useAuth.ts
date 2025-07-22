7f2af0bca170ba2bcaec290d59b484d3f125f9fe
kanori1022 <i.like_96neko54@ezweb.ne.jp>
2025-07-22 22:20:06 +0900

    認証処理を追加

--- hooks/auth/useAuth.ts
+++ hooks/auth/useAuth.ts
@@ -1,10 +1,8 @@
 "use client";
 
-import { FirebaseError, getApps, initializeApp } from 'firebase/app'; // Firebaseアプリの初期化を行うためのinitializeApp関数を'firebase/app'からインポート
-import { getAuth, onAuthStateChanged, signOut, User } from 'firebase/auth'; // Firebase Authenticationを使用するためのgetAuth関数を'firebase/auth'からインポート
-import { useEffect, useState } from 'react';
-import { useAxios } from '../axios/useAxios';
-
+import { FirebaseError, getApps, initializeApp } from "firebase/app"; // Firebaseアプリの初期化を行うためのinitializeApp関数を'firebase/app'からインポート
+import { getAuth, onAuthStateChanged, signOut, User } from "firebase/auth"; // Firebase Authenticationを使用するためのgetAuth関数を'firebase/auth'からインポート
+import { useEffect, useState } from "react";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -20,16 +18,13 @@ if (!getApps().length) {
   initializeApp(firebaseConfig);
 }
 
-
 const useAuth = () => {
   const app = initializeApp(firebaseConfig); // Firebaseアプリの初期化
   const auth = getAuth(app); // Firebase Authenticationの認証オブジェクトを取得
-  const axios = useAxios(); // axiosのインスタンスを取得
 
   const [loginUser, setLoginUser] = useState<User | null>(null);
   const [token, setToken] = useState<string | null>(null); // トークンを保持するための変数を定義し、初期値をnullに設定
   const [isWaiting, setIsWaiting] = useState<boolean>(false); // ログイン状態の変化を監視するための変数を定義し、初期値をtrueに設定
-
 
   const handleSignOut = async () => {
     try {
@@ -44,31 +39,27 @@ const useAuth = () => {
   // ※ログインのセットより初期描画の方が早いため、
   // これをしないと、初期描画時にログイン状態がセットされず、
   // 初期描画時にログインが必要なページを表示できない。
-   useEffect(() => {
-     setIsWaiting(true);
+  useEffect(() => {
+    setIsWaiting(true);
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setLoginUser(user);
       }
-       setIsWaiting(false);
-     });
+      setIsWaiting(false);
+    });
   }, [auth]);
-
-
 
   useEffect(() => {
     if (loginUser) {
       loginUser.getIdToken(true).then((idToken) => {
-        axios.defaults.headers.common["Authorization"] = `Bearer ${idToken}`;
         setToken(idToken);
       });
     }
-  }, [loginUser, axios]);
+  }, [loginUser]);
 
   return { auth, loginUser, token, isWaiting, handleSignOut };
 };
 
 export default useAuth;
-
 
 // https://zenn.dev/hisho/books/617d8f9d6bd78b/viewer/chapter8#firebase-authentication%E3%81%A8%E3%82%B5%E3%82%A4%E3%83%B3%E3%82%A2%E3%82%A6%E3%83%88%E3%82%92%E7%B4%90%E4%BB%98%E3%81%91%E3%82%88%E3%81%86
--- hooks/axios/useAxios.ts
+++ hooks/axios/useAxios.ts
@@ -1,16 +1,18 @@
 "use client";
 
-import axios from 'axios';
+import axios from "axios";
+import useAuth from "../auth/useAuth";
 
 export const useAxios = () => {
+  const { token } = useAuth();
   const instance = axios.create({
-        baseURL: process.env.NEXT_PUBLIC_API_HOST,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        // withCredentials: true,
-  })
+    baseURL: process.env.NEXT_PUBLIC_API_HOST,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, // トークンをヘッダーに追加
+    },
+    // withCredentials: true,
+  });
 
-      return instance
-}
- 
+  return instance;
+};
