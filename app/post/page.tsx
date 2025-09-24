69d24df6c3b150606ee97ac9444fa5f5e3e11fc9
kanori1022 <>
2025-09-24 20:22:34 +0900

    デプロイエラー修正

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -7,6 +7,7 @@ import useAuth from "@/hooks/auth/useAuth";
 import { CombinationParams } from "@/types/combination";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
@@ -168,9 +169,11 @@ export default function Post() {
             <div className="flex justify-center">
               <div className="w-48 h-48 border-2 border-dashed border-gray-300 rounded-lg overflow-hidden hover:border-gray-400 transition-colors">
                 {previewUrl ? (
-                  <img
+                  <Image
                     src={previewUrl}
                     alt="選択された画像"
+                    width={400}
+                    height={300}
                     className="w-full h-full object-cover cursor-pointer"
                     onClick={() => {
                       inputRef.current?.click();
